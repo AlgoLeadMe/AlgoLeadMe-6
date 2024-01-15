11a67b0fd626db85c07e8486cfc0dc8da8f4dd09
김민석|Minseok Kim <alstjr7437@naver.com>
2024-01-15 12:14:12 +0900

    2024-01-15 전화번호 목록

---
+++ alstjr7437/해시/전화번호 목록.py
@@ -0,0 +1,25 @@
+# 1차 실패(시간 초과)
+def solution(phone_book):
+    for i in range(len(phone_book)):
+        a = phone_book.pop(i)
+        for j in phone_book:
+            if a in j : 
+                return False
+        phone_book.insert(i, a)
+    return True
+    
+# 2차 실패
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] in phone_book[i+1] : 
+            return False
+    return True
+    
+# 최종
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i]==phone_book[i+1][:len(phone_book[i])]:
+            return False
+    return True
