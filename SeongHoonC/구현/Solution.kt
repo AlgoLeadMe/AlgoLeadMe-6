412a37d5eda6e4610788aabd1974c01fa4260c76
vrexpert <vrexpert2302@gmail.com>
2024-01-24 14:13:20 +0900

    2024-01-24 마법의 엘리베이터 solved

---
+++ SeongHoonC/구현/Solution.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun solution(storey: Int): Int {
+        var number = storey
+        var count = 0
+        while (number > 0) {
+            val remainder = number % 10
+            when {
+                remainder > 5 -> {
+                    count += (10 - remainder)
+                    number += 10
+                }
+
+                remainder < 5 -> count += remainder
+
+                (number / 10) % 10 > 4 -> {
+                    number += 10
+                    count += remainder
+                }
+
+                else -> count += remainder
+            }
+            number /= 10
+        }
+        return count
+    }
+}
