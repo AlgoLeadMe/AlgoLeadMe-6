70f4646eb9634376200cbce8d58af469a1c7be2b
JangHongJoon <wkdghdwns19969@gmail.com>
2024-02-26 23:42:42 +0900

    2024.02.26 solved

--- wkdghdwns199/스택_큐_덱/ACM-2346.py
+++ wkdghdwns199/스택_큐_덱/ACM-2346.py
@@ -1,31 +1,17 @@
-N = int(input())
-paper = list(map(int, input().split()))
-pop_check = [0 for _ in range(N+1)]
-pop_check[0], pop_check[1] = 1,1
-queue = [1]
+from collections import deque
 
+n = int(input())
+deq = deque(enumerate(map(int, input().split())))
+answer = []
 
-while len(queue) != N :
-    # print (queue[len(queue)-1], paper[queue[len(queue)-1]-1])
-    cnt = queue[len(queue)-1] + paper[queue[len(queue)-1]-1]
-    if cnt <= 0 : 
-        cnt =  (queue[len(queue)-1] - paper[queue[len(queue)-1]-1]) - N
-    elif cnt > N :
-        cnt = (queue[len(queue)-1] + paper[queue[len(queue)-1]-1]) - N
-    # print(cnt)
+while deq:
+    cnt, paper = deq.popleft()
+    answer.append(cnt + 1)
 
-    while pop_check[cnt] == 1 :
-        if paper[queue[len(queue)-1]-1] > 0 : 
-            cnt+=1
-            if (cnt > N) : cnt = 1
-        else : 
-            cnt-=1
-            if (cnt == 0) : cnt = N
-        # print('!', cnt)
-    
-    pop_check[cnt] = 1
-    queue.append(cnt)
+    if paper > 0:
+        deq.rotate(-(paper - 1))
+    elif paper < 0:
+        deq.rotate(-paper)
 
-
-for balloon in queue :
+for balloon in answer :
     print(balloon, end=' ')
