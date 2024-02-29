eaebe4f82df1abcb77ac61731aab3749169eddd0
JangHongJoon <wkdghdwns19969@gmail.com>
2024-02-29 16:04:16 +0900

    2024.02.29 solved

--- wkdghdwns199/리뷰풀이/ACM-18258.py
+++ wkdghdwns199/리뷰풀이/ACM-18258.py
@@ -1,19 +1,19 @@
 import sys
-
+from collections import deque
 N = int(input())
-queue = list()
+queue = deque()
 for _ in range(N):
     _str = sys.stdin.readline().split()
     if 'push' == _str[0] :
         queue.append(_str[1])
     elif 'pop' == _str[0] :
         if len(queue) == 0 : print(-1)
-        else : print(queue.pop(0))
+        else : print(queue.popleft())
     elif 'size' == _str[0] :
         print(len(queue))
     elif 'empty' == _str[0] :
-        if len(queue) == 0 : print(0)
-        else : print(1)
+        if len(queue) == 0 : print(1)
+        else : print(0)
     elif 'front' == _str[0] :
         if len(queue) == 0 : print(-1)
         else : print(queue[0])
